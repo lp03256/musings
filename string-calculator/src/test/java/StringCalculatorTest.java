7b60a4d37667d3ca0ca93dba02780b33669ecadd
Anadi Misra <anadi.misra@agilityroots.com>
2018-09-28 11:47:04 +0530

    Kata completed till step 2

--- string-calculator/src/test/java/StringCalculatorTest.java
+++ string-calculator/src/test/java/StringCalculatorTest.java
@@ -10,17 +10,31 @@ public class StringCalculatorTest {
     }
 
     @Test
-    public void testSumOfSingleNuberIsTheNumberItself(){
+    public void testSumOfSingleNumberIsTheNumberItself(){
         int result = Add("5");
         Assert.assertEquals("Sum of a number should be number itself", 5, result);
     }
-    
+
+    @Test
+    public void canSumTwoNumbers(){
+        int result = Add("4,5");
+        Assert.assertEquals("Sum of 4,5 is 9", 9, result);
+    }
+
     private int Add(String numbers){
         if (numbers.isEmpty())
             return 0;
         else if(numbers.length() == 1)
             return Integer.parseInt(numbers);
-        else
-            return -1;
+        else{
+            int sum = 0;
+            String [] toAdd = numbers.split(",");
+            for (String number:
+                 toAdd) {
+                sum += Integer.parseInt(number);
+            }
+            return sum;
+        }
+
     }
 }
