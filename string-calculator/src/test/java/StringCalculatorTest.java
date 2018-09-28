55f7eb86a59f26eac64388fe18ac864a6ad1db1a
Anadi Misra <anadi.misra@agilityroots.com>
2018-09-28 11:39:32 +0530

    fixed false positive, test method now has logic for empty string and single digit

--- string-calculator/src/test/java/StringCalculatorTest.java
+++ string-calculator/src/test/java/StringCalculatorTest.java
@@ -9,16 +9,18 @@ public class StringCalculatorTest {
         Assert.assertEquals("Sum of null string should be zero", 0, result);
     }
 
-    /*
-     * False Positive
-     */
     @Test
     public void testSumOfSingleNuberIsTheNumberItself(){
-        int result = Add("0");
-        Assert.assertEquals("Sum of a number should be number itself", 0, result);
+        int result = Add("5");
+        Assert.assertEquals("Sum of a number should be number itself", 5, result);
     }
-
+    
     private int Add(String numbers){
-        return 0;
+        if (numbers.isEmpty())
+            return 0;
+        else if(numbers.length() == 1)
+            return Integer.parseInt(numbers);
+        else
+            return -1;
     }
 }
