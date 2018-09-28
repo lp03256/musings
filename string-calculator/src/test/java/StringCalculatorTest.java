12a09c834243501d63ed547e9656f2c2b9b13d2d
Anadi Misra <anadi.misra@agilityroots.com>
2018-09-28 11:28:53 +0530

    Added a test to show false positives with TDD
    fixed string message of false positive

--- string-calculator/src/test/java/StringCalculatorTest.java
+++ string-calculator/src/test/java/StringCalculatorTest.java
@@ -4,11 +4,18 @@ import org.junit.Test;
 public class StringCalculatorTest {
 
     @Test
-    public void testAddTwoNumbers(){
-
+    public void testSumOfEmptyStringIsZero(){
         int result = Add("");
         Assert.assertEquals("Sum of null string should be zero", 0, result);
+    }
 
+    /*
+     * False Positive
+     */
+    @Test
+    public void testSumOfSingleNuberIsTheNumberItself(){
+        int result = Add("0");
+        Assert.assertEquals("Sum of a number should be number itself", 0, result);
     }
 
     private int Add(String numbers){
