4c5b181f516f91287797804c9bdb9de1d372c94d
Anadi Misra <anadi.misra@agilityroots.com>
2018-10-03 10:54:40 +0530

    refactored initialise logic into single method and removed initial unit test as it does not carry any value now

--- minesweeper/src/test/java/MineFieldReaderTest.java
+++ minesweeper/src/test/java/MineFieldReaderTest.java
@@ -9,26 +9,20 @@ import static org.junit.Assert.*;
 public class MineFieldReaderTest {
 
     @Test
-    public void canInitialiseMineField() {
-
-        char [][] board = initialiseMineField("3 3");
-        assertEquals("Mine field size is 3X3", 3, board.length);
-    }
-
-    @Test
     public void canInitializeMineFieldFromFile() throws IOException {
         FileWriter fileWriter = new FileWriter("mines.ini");
         fileWriter.write("3 3" + System.getProperty("line.separator") );
         fileWriter.close();
-        assertEquals("First Line of file is 3 3", "3 3", readMineFieldFile("mines.ini"));
+        char [][] board = initialiseMineField("mines.ini");
+        assertEquals("Mine field size is 3X3", 3, board.length);
+        new File("mines.ini").delete();
+
     }
 
-    private char[][] initialiseMineField(String dimensions) {
-        String [] size = dimensions.split(" ");
+    private char[][] initialiseMineField(String pathToMineFieldFile) throws IOException{
+        String [] size = Files.lines(new File(pathToMineFieldFile).toPath()).findFirst().get().split(" ");
         return new char [Integer.parseInt(size[0])][Integer.parseInt(size[0])];
     }
 
-    private String readMineFieldFile(String pathToMineFieldFile) throws IOException {
-        return Files.lines(new File(pathToMineFieldFile).toPath()).findFirst().get();
-    }
+
 }
