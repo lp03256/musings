ab22e93d3e4caabd88b91dee4ce99db3af4c09ea
Anadi Misra <anadi.misra@agilityroots.com>
2018-10-03 10:49:19 +0530

    should initialize mine field from first line of the text file

--- minesweeper/src/test/java/MineFieldReaderTest.java
+++ minesweeper/src/test/java/MineFieldReaderTest.java
@@ -1,4 +1,8 @@
 import org.junit.Test;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
 
 import static org.junit.Assert.*;
 
@@ -11,8 +15,20 @@ public class MineFieldReaderTest {
         assertEquals("Mine field size is 3X3", 3, board.length);
     }
 
+    @Test
+    public void canInitializeMineFieldFromFile() throws IOException {
+        FileWriter fileWriter = new FileWriter("mines.ini");
+        fileWriter.write("3 3" + System.getProperty("line.separator") );
+        fileWriter.close();
+        assertEquals("First Line of file is 3 3", "3 3", readMineFieldFile("mines.ini"));
+    }
+
     private char[][] initialiseMineField(String dimensions) {
         String [] size = dimensions.split(" ");
         return new char [Integer.parseInt(size[0])][Integer.parseInt(size[0])];
+    }
+
+    private String readMineFieldFile(String pathToMineFieldFile) throws IOException {
+        return Files.lines(new File(pathToMineFieldFile).toPath()).findFirst().get();
     }
 }
