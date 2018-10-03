328824396170caf224871effc83245531a072651
Anadi Misra <anadi.misra@agilityroots.com>
2018-10-03 10:37:44 +0530

    basic initialise method to make test pass

--- minesweeper/src/test/java/MineFieldReaderTest.java
+++ minesweeper/src/test/java/MineFieldReaderTest.java
@@ -5,9 +5,14 @@ import static org.junit.Assert.*;
 public class MineFieldReaderTest {
 
     @Test
-    public void canInitialiseMineField(){
+    public void canInitialiseMineField() {
 
         char [][] board = initialiseMineField("3 3");
         assertEquals("Mine field size is 3X3", 3, board.length);
+    }
+
+    private char[][] initialiseMineField(String dimensions) {
+        String [] size = dimensions.split(" ");
+        return new char [Integer.parseInt(size[0])][Integer.parseInt(size[0])];
     }
 }
