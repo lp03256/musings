3c89ee388fd7dcd94ee3a06be27c9d4d5918f3bf
Anadi Misra <anadi.misra@agilityroots.com>
2018-10-12 23:12:38 +0530

    basic failing test for adding empty string

---
+++ csharp/StringCalculator-sln/StringCalculator.Tests/StringCalculatorUnitTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringCalculator.Tests
+{
+    [TestClass]
+    public class StringCalculatorUnitTests
+    {
+        [TestMethod]
+        public void TestSumOfEmptyStringIsZero()
+        {
+            StringCalculator.Addition calculator = new StringCalculator.Addition();
+        }
+    }
+}
