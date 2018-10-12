32a941ad05dadc062291fdbb50be63bae2439f25
Anadi Misra <anadi.misra@agilityroots.com>
2018-10-12 22:52:37 +0530

    moved projects to language folders

--- java/minesweeper/src/test/java/com/agilityroots/minesweeper/MineFieldReaderTest.java
+++ java/minesweeper/src/test/java/com/agilityroots/minesweeper/MineFieldReaderTest.java
@@ -1,12 +1,10 @@
 package com.agilityroots.minesweeper;
 
-import com.agilityroots.minesweeper.MineFieldReader;
 import org.junit.Test;
+
 import java.io.IOException;
-import java.util.List;
 
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class MineFieldReaderTest {
 
@@ -14,7 +12,7 @@ public class MineFieldReaderTest {
     public void canInitializeMineFieldFromFile() throws IOException {
         String path = getClass().getClassLoader().getResource("mines.ini").getPath();
         MineFieldReader mineFieldReader = new MineFieldReader();
-        char [][] mineField = mineFieldReader.initialiseMineField(path);
+        char[][] mineField = mineFieldReader.initialiseMineField(path);
         assertEquals("This mine field as 3 rows", 3, mineField.length);
         assertEquals("First cell is a mine", '*', mineField[0][0]);
     }
