96c3543d8e5b3e7091ebcfe7889aeab7e3dadcba
Anadi Misra <anadi.misra@agilityroots.com>
2018-10-13 10:44:07 +0530

    added failing test for sum of single number

--- python/string_calculator/tests/test_calculator.py
+++ python/string_calculator/tests/test_calculator.py
@@ -9,5 +9,9 @@ class CalculatorTest(unittest.TestCase):
 
     def test_sum_of_empty_string_is_zero(self):
         calc = Calculator()
-        self.assertEqual(calc.add(""), 0 , "Sum of empty string is zero")     
+        self.assertEqual(calc.add(""), 0 , "Sum of empty string is zero")   
+
+    def test_sum_of_number_is_itself(self):
+        calc = Calculator()
+        self.assertEqual(calc.add("5"), 5 , "Sum of number is itself")     
         
