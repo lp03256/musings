8616dceba9b082c71b19a4710bebc3ab4fcbe7ea
Anadi Misra <anadi.misra@agilityroots.com>
2018-10-13 10:25:46 +0530

    added empty test that does nothing

--- python/string_calculator/tests/calculator_tests.py
+++ python/string_calculator/tests/calculator_tests.py
@@ -1,2 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import unittest
+import sys
+
+from calculator.calculator import Calculator
+
+class CalculatorTest(unittest.TestCase):
+
+    def test_sum_of_empty_string_is_zero(self):
+        calc = Calculator()
+        
