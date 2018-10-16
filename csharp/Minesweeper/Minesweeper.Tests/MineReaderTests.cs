dc5046713ad80c74e47e801d44233750a6792a67
Anadi Misra <anadi.misra@agilityroots.com>
2018-10-16 22:27:07 +0530

    added first failing test

--- csharp/Minesweeper/Minesweeper.Tests/MineReaderTests.cs
+++ csharp/Minesweeper/Minesweeper.Tests/MineReaderTests.cs
@@ -9,6 +9,8 @@ namespace Minesweeper.Tests
         public void TestInitialisesMine()
         {
             Minesweeper.MineReader reader = new Minesweeper.MineReader();
+            char [,] mine = reader.InitialiseMine("4 4");
+            Assert.AreEqual(16, mine.Length, "Creates a mine of length 16 for input \"4 4\"");
         }
     }
 }
