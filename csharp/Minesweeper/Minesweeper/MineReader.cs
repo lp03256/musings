d4cf16dcfea2d4a4b0814978ea917927bd493c8f
Anadi Misra <anadi.misra@agilityroots.com>
2018-10-16 22:30:22 +0530

    fixed failing test for mine field initialisation

--- csharp/Minesweeper/Minesweeper/MineReader.cs
+++ csharp/Minesweeper/Minesweeper/MineReader.cs
@@ -4,5 +4,10 @@ namespace Minesweeper
 {
     public class MineReader
     {
+        public char[,] InitialiseMine(String size)
+        {
+            String[] dimensions = size.Split(' ');
+            return new char[Int32.Parse(dimensions[0]), Int32.Parse(dimensions[1])];
+        }
     }
 }
