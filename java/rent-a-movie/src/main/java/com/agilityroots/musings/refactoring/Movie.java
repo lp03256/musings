6e9e4630561436342643bed7b70b3482997fab6d
Anadi Misra <anadi.misra@agilityroots.com>
2019-04-08 16:24:44 +0530

    added Price class for price codes
    Signed-off-by: Anadi Misra <anadi.misra@agilityroots.com>

--- java/rent-a-movie/src/main/java/com/agilityroots/musings/refactoring/Movie.java
+++ java/rent-a-movie/src/main/java/com/agilityroots/musings/refactoring/Movie.java
@@ -6,10 +6,11 @@ public class Movie {
     public static final int NEW_RELEASE = 1;
     private String _title;
     private int _priceCode;
+    private Price _price;
 
     public Movie(String title, int priceCode) {
         _title = title;
-        _priceCode = priceCode;
+        setPriceCode(priceCode);
     }
 
     public String getTitle() {
@@ -17,11 +18,25 @@ public class Movie {
     }
 
     public int getPriceCode() {
-        return _priceCode;
+
+        return _price.getPriceCode();
     }
 
     public void setPriceCode(int priceCode) {
-        _priceCode = priceCode;
+
+        switch (priceCode) {
+            case REGULAR:
+                _price = new RegularPrice();
+                break;
+            case CHILDRENS:
+                _price = new ChildrensPrice();
+                break;
+            case NEW_RELEASE:
+                _price = new NewReleasePrice();
+                break;
+            default:
+                throw new IllegalArgumentException("Incorrect Price Code");
+        }
     }
 
     double getCharge(int daysRented) {
