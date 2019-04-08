cfff674290545312dbe24689dcf896864518599f
Anadi Misra <anadi.misra@agilityroots.com>
2019-04-08 15:04:44 +0530

    moving frequent renter calculation to Rental class
    Signed-off-by: Anadi Misra <anadi.misra@agilityroots.com>

--- java/rent-a-movie/src/main/java/com/agilityroots/musings/refactoring/Customer.java
+++ java/rent-a-movie/src/main/java/com/agilityroots/musings/refactoring/Customer.java
@@ -27,7 +27,7 @@ class Customer {
         while (rentals.hasMoreElements()) {
             double thisAmount = 0;
             Rental each = (Rental) rentals.nextElement();
-            frequentRenterPoints += getFrequentRenterPoints(each);
+            frequentRenterPoints += each.getFrequentRenterPoints();
 
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" +
@@ -41,18 +41,6 @@ class Customer {
                 +
                 " frequent renter points";
         return result;
-    }
-
-    private int getFrequentRenterPoints(Rental each) {
-        int frequentRenterPoints = 0;
-        // add frequent renter points
-        frequentRenterPoints++;
-        // add bonus for a two day new release rental
-        if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-                &&
-                each.getDaysRented() > 1)
-            frequentRenterPoints++;
-        return frequentRenterPoints;
     }
 
 }
