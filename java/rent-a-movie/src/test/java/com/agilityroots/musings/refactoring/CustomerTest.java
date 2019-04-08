107c4e63ddb975786ceb28d093ed2a817b28080b
Anadi Misra <anadi.misra@agilityroots.com>
2019-04-08 15:46:09 +0530

    added html statement method
    Signed-off-by: Anadi Misra <anadi.misra@agilityroots.com>

--- java/rent-a-movie/src/test/java/com/agilityroots/musings/refactoring/CustomerTest.java
+++ java/rent-a-movie/src/test/java/com/agilityroots/musings/refactoring/CustomerTest.java
@@ -1,22 +1,29 @@
 package com.agilityroots.musings.refactoring;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CustomerTest {
 
-    @Test
-    public void testCustomerStatement() {
+    Customer stan;
+
+    @Before
+    public void setup() {
         Movie snatch = new Movie("Snatch", Movie.REGULAR);
         Movie shazam = new Movie("Shazam", Movie.NEW_RELEASE);
 
         Rental snatchRental = new Rental(snatch, 3);
         Rental shazamRental = new Rental(shazam, 3);
 
-        Customer stan = new Customer("Stan Lee");
+        stan = new Customer("Stan Lee");
         stan.addRental(snatchRental);
         stan.addRental(shazamRental);
+    }
+
+    @Test
+    public void testCustomerStatement() {
 
         String statement = stan.statement();
         String expected = "Rental Record for Stan Lee\n" +
@@ -28,5 +35,19 @@ public class CustomerTest {
         assertThat(statement).contains("Stan Lee");
         assertThat(statement).isEqualTo(expected);
 
+    }
+
+    @Test
+    public void testCustomerHtmlStatement() {
+
+        String htmlStatement = stan.htmlStatement();
+        String expected = "<H1>Rentals for <EM>Stan Lee</EM></H1><P>\n" +
+                "Snatch: 3.5<BR>\n" +
+                "Shazam: 9.0<BR>\n" +
+                "<P>You owe <EM>12.5</EM></P>\n" +
+                "On this rental you earned <EM>3</EM> frequent renter points</P>";
+
+        assertThat(htmlStatement).contains("Stan Lee");
+        assertThat(htmlStatement).isEqualTo(expected);
     }
 }
