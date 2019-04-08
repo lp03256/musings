721d97f6dbe8b25fa10c76fa68c0224786c9c74a
Anadi Misra <anadi.misra@agilityroots.com>
2019-04-08 15:56:14 +0530

    moved rental calculation to movie class
    Signed-off-by: Anadi Misra <anadi.misra@agilityroots.com>

---
+++ java/rent-a-movie/src/test/java/com/agilityroots/musings/refactoring/MovieTest.java
@@ -0,0 +1,54 @@
+package com.agilityroots.musings.refactoring;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MovieTest {
+
+
+    @Test
+    public void testRentalForNewRelease() {
+        Movie movie = new Movie("Some Moview", Movie.NEW_RELEASE);
+
+        double charge = movie.getCharge(2);
+
+        assertThat(charge).isEqualTo(6.0);
+    }
+
+    @Test
+    public void testRentalForRegularMovie() {
+        Movie movie = new Movie("Some Moview", Movie.REGULAR);
+
+        double charge = movie.getCharge(2);
+
+        assertThat(charge).isEqualTo(2.0);
+    }
+
+    @Test
+    public void testRentalForRegularMovieMoreThanTwoDays() {
+        Movie movie = new Movie("Some Moview", Movie.REGULAR);
+
+        double charge = movie.getCharge(3);
+
+        assertThat(charge).isEqualTo(3.5);
+    }
+
+    @Test
+    public void testRentalForChildrensMovie() {
+        Movie movie = new Movie("Some Moview", Movie.CHILDRENS);
+
+        double charge = movie.getCharge(2);
+
+        assertThat(charge).isEqualTo(1.5);
+    }
+
+    @Test
+    public void testRentalForChildrenMovieMoreThanThreeDays() {
+        Movie movie = new Movie("Some Moview", Movie.CHILDRENS);
+
+        double charge = movie.getCharge(4);
+
+        assertThat(charge).isEqualTo(3.0);
+    }
+}
--- java/rent-a-movie/src/test/java/com/agilityroots/musings/refactoring/RentalTest.java
+++ java/rent-a-movie/src/test/java/com/agilityroots/musings/refactoring/RentalTest.java
@@ -7,51 +7,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class RentalTest {
 
     @Test
-    public void testRentalForNewRelease() {
-        Rental rental = new Rental(new Movie("Some Moview", Movie.NEW_RELEASE), 2);
-
-        double charge = rental.getCharge();
-
-        assertThat(charge).isEqualTo(6.0);
-    }
-
-    @Test
-    public void testRentalForRegularMovie() {
-        Rental rental = new Rental(new Movie("Some Moview", Movie.REGULAR), 2);
-
-        double charge = rental.getCharge();
-
-        assertThat(charge).isEqualTo(2.0);
-    }
-
-    @Test
-    public void testRentalForRegularMovieMoreThanTwoDays() {
-        Rental rental = new Rental(new Movie("Some Moview", Movie.REGULAR), 3);
-
-        double charge = rental.getCharge();
-
-        assertThat(charge).isEqualTo(3.5);
-    }
-
-    @Test
-    public void testRentalForChildrensMovie() {
-        Rental rental = new Rental(new Movie("Some Moview", Movie.CHILDRENS), 2);
-
-        double charge = rental.getCharge();
-
-        assertThat(charge).isEqualTo(1.5);
-    }
-
-    @Test
-    public void testRentalForChildrenMovieMoreThanThreeDays() {
-        Rental rental = new Rental(new Movie("Some Moview", Movie.CHILDRENS), 4);
-
-        double charge = rental.getCharge();
-
-        assertThat(charge).isEqualTo(3.0);
-    }
-
-    @Test
     public void testFrequentRenterPointsForNewReleases() {
         Rental rental = new Rental(new Movie("Some Moview", Movie.NEW_RELEASE), 2);
 
