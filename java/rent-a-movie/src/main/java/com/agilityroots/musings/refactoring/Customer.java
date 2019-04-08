c327be660cf5645cedda67addd2bda360cbd738d
Anadi Misra <anadi.misra@agilityroots.com>
2019-04-08 14:55:12 +0530

    removed redundant local variable thisAmount
    Signed-off-by: Anadi Misra <anadi.misra@agilityroots.com>

--- java/rent-a-movie/src/main/java/com/agilityroots/musings/refactoring/Customer.java
+++ java/rent-a-movie/src/main/java/com/agilityroots/musings/refactoring/Customer.java
@@ -27,7 +27,7 @@ class Customer {
         while (rentals.hasMoreElements()) {
             double thisAmount = 0;
             Rental each = (Rental) rentals.nextElement();
-            thisAmount = each.getCharge();
+
             // add frequent renter points
             frequentRenterPoints++;
             // add bonus for a two day new release rental
@@ -36,8 +36,8 @@ class Customer {
                     each.getDaysRented() > 1) frequentRenterPoints++;
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" +
-                    String.valueOf(thisAmount) + "\n";
-            totalAmount += thisAmount;
+                    String.valueOf(each.getCharge()) + "\n";
+            totalAmount += each.getCharge();
         }
         //add footer lines
         result += "Amount owed is " + String.valueOf(totalAmount) +
